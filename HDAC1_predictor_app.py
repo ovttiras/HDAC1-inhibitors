1eda37940b0d8492430c9ec8ee19b695ea9269a6
ovttiras <ovttiras@yandex.ru>
2022-05-04 23:02:30 +0300

    Update HDAC1_predictor_app.py

--- HDAC1_predictor_app.py
+++ HDAC1_predictor_app.py
@@ -630,6 +630,15 @@ if models_option == 'RDKit':
                     render_mol(blk)
                     st.write('You can use the scroll wheel on your mouse to zoom in or out a 3D structure of compound')
                     predictions = pd.DataFrame({'SMILES': smi, 'HDAC1 activity': pred_consensus[i],'Applicability domain (AD)': cpd_AD_vs[i]}, index=[0])
+                    # CSS to inject contained in a string
+                    hide_dataframe_row_index = """
+                                <style>
+                                .row_heading.level0 {display:none}
+                                .blank {display:none}
+                                </style>
+                                """
+                    # Inject CSS with Markdown
+                    st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
                     st.dataframe(predictions)           
 
                     # st.write('**Smiles for compound number **'+ str(i+1) + '**:**', smi)
